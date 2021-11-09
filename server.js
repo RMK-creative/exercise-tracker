eec5bfd76a1d73dfe87a31d9798069805792f944
Rachael Kunz <rachaelmkunz@gmail.com>
2021-11-09 15:59:53 +0100

    add view engine & home route

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ require("dotenv").config({ path: "./config/.env" });
 
 connectDB();
 
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/*
+        ROUTES
+*/
+app.use("/", require("./routes/home"));
+// app.use("/sessions", require("./routes/sessions"));
+
 /*
         LISTEN
 */
