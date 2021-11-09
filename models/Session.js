5d0d100b527aff767f72ae205b5f6ee134097b44
Rachael Kunz <rachaelmkunz@gmail.com>
2021-11-09 20:57:00 +0100

    remove required field as it is giving error
    Tested removing it, no error & comms with db OK.

--- models/Session.js
+++ models/Session.js
@@ -3,20 +3,19 @@ const mongoose = require("mongoose");
 const SessionSchema = new mongoose.Schema({
   type: {
     type: String,
-    required: true,
   },
   date: {
     type: Date,
     default: Date.now(),
-    // required: true,
   },
   hr: {
     type: Number,
-    // required: true,
   },
   calories: {
     type: Number,
-    // required: true,
+  },
+  duration: {
+    type: Number,
   },
 });
 module.exports = mongoose.model("Session", SessionSchema);
