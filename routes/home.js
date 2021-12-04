7d3a65d74be24909bb92023445a5711b1055917d
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-04 19:58:31 +0100

    add routes for id & getting session data

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
+router.get("/session/addSession", homeController.getAddSessionView);
 
 module.exports = router;
--- routes/sessions.js
+++ routes/sessions.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const sessionsController = require("../controllers/sessions");
+const sessionDataController = require("../controllers/sessionData");
 
 router.get("/", sessionsController.getSessions);
-router.post("/createSession", sessionsController.createSession); 
+router.get("/:id", sessionDataController.getSessionData);
+router.post("/createSession", sessionsController.createSession);
 
 module.exports = router;
