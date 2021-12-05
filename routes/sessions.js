6d8104a33bde03e64785460175e5e2b2c95938c9
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-05 16:25:59 +0100

    add router for deleteSession

--- routes/sessions.js
+++ routes/sessions.js
@@ -5,5 +5,6 @@ const sessionsController = require("../controllers/sessions");
 router.get("/", sessionsController.getSessions);
 router.get("/:id", sessionsController.getSessionData);
 router.post("/createSession", sessionsController.createSession);
+router.delete("/:id", sessionsController.deleteSession);
 
 module.exports = router;
